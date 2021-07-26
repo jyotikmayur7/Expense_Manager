66b0f0bdbffc67ab069cd64f7023eaedb7045301
Jyotik Mayur <heyjyotik@gmail.com>
2021-07-26 20:47:30 +0100

    Connect listView to the code and created an expensesList array  which contains different sorts of expeneses

--- app/src/main/java/com/example/expensemanager/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.expensemanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val expeses: ListView = findViewById(R.id.expenses)
+        val expensesList = arrayOf("Groceries", "Transportation", "Rent", "Cell Phone", "Utility Bills", "Insurance")
+
+
     }
 }
