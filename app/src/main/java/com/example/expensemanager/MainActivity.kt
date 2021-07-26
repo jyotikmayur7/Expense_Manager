7d45e3f7240a026933e6a9c380ad1048273fad5d
Jyotik Mayur <heyjyotik@gmail.com>
2021-07-26 21:00:25 +0100

    Implemented onClickListener on the ListView, show text when the respective adapter is clicked

--- app/src/main/java/com/example/expensemanager/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.expensemanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +18,10 @@ class MainActivity : AppCompatActivity() {
 
         val expensesAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, expensesList)
         expenses.adapter = expensesAdapter
+
+        expenses.setOnItemClickListener{adapterView, view, position, id ->
+            val expense: TextView = view as TextView
+            Toast.makeText(this, expense.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
