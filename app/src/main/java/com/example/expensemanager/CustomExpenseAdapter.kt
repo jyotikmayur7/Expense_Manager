2b9ebd704a9da9e72c94f3cc5c2ca8392818bd7c
Jyotik Mayur <heyjyotik@gmail.com>
2021-07-27 15:15:48 +0100

    Implemented getCount(), getItem(), getItemId() methods in the CustoExpenseAdapter class

--- app/src/main/java/com/example/expensemanager/CustomExpenseAdapter.kt
+++ app/src/main/java/com/example/expensemanager/CustomExpenseAdapter.kt
@@ -4,17 +4,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class CustomExpenseAdapter(expenses: ArrayList<Expenses>): BaseAdapter() {
+class CustomExpenseAdapter(val expenses: ArrayList<Expenses>): BaseAdapter() {
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return expenses.size
     }
 
-    override fun getItem(p0: Int): Any {
-        TODO("Not yet implemented")
+    override fun getItem(position: Int): Any {
+        return expenses.get(position)
     }
 
-    override fun getItemId(p0: Int): Long {
-        TODO("Not yet implemented")
+    override fun getItemId(position: Int): Long {
+        return expenses.get(position).item.hashCode().toLong()
     }
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
