f373cbf5853175731eeef19d80696b4833594298
Jyotik Mayur <heyjyotik@gmail.com>
2021-07-27 15:05:58 +0100

    added companion object in the Expenses data class to format cost and total cost, and added created expensesList

--- app/src/main/java/com/example/expensemanager/Expenses.kt
+++ app/src/main/java/com/example/expensemanager/Expenses.kt
@@ -1,3 +1,24 @@
 package com.example.expensemanager
 
-data class Expenses(val item: String, val cost: Int)
+import java.text.NumberFormat
+import java.util.*
+
+data class Expenses(val item: String, val cost: Float){
+    fun getFormattedPrice(): String = formatter.format(cost)
+
+    companion object {
+        private val formatter: NumberFormat = NumberFormat.getCurrencyInstance()
+
+        init {
+            formatter.currency = Currency.getInstance("INR")
+        }
+
+        fun total(expenses : ArrayList<Expenses>) : String {
+            var total: Float = 0f
+            expenses.forEach {
+                total += it.cost
+            }
+            return formatter.format(total)
+        }
+    }
+}
--- app/src/main/java/com/example/expensemanager/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.expensemanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,14 +11,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val expenses: ListView = findViewById(R.id.expenses)
-        val expensesList = arrayOf("Groceries", "Transportation", "Rent", "Cell Phone", "Utility Bills", "Insurance")
+        val totalCost: TextView = findViewById(R.id.totalExpensesAmount)
+
+        val expensesList= arrayListOf(Expenses("Groceries",5000f),
+                                Expenses("Transportation", 8000f),
+                                Expenses("Rent", 50000f),
+                                Expenses("Cell Phone",800f),
+                                Expenses("Utility Bills", 6500f),
+                                Expenses("Insurance", 5000f))
 
         val expensesAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, expensesList)
         expenses.adapter = expensesAdapter
 
-        expenses.setOnItemClickListener{adapterView, view, position, id ->
-            val expense: TextView = view as TextView
-            Toast.makeText(this, expense.text, Toast.LENGTH_SHORT).show()
-        }
+        totalCost.text = "₹" + Expenses.total(expensesList)
+
+//        expenses.setOnItemClickListener{adapterView, view, position, id ->
+//            val expense: TextView = view as TextView
+//            Toast.makeText(this, expense.text, Toast.LENGTH_SHORT).show()
+//        }
     }
 }
