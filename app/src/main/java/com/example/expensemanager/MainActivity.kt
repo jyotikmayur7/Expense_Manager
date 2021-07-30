296d8289cdebebfa6ee98fc2cdde74893834f94f
Jyotik Mayur <heyjyotik@gmail.com>
2021-07-30 19:43:25 +0100

    Implented onClick listener on Reset button which calls resetList() function which shows an alert diaglog and does respective operation

--- app/src/main/java/com/example/expensemanager/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     val expensesList = mutableListOf<Expenses>()
@@ -18,11 +19,33 @@ class MainActivity : AppCompatActivity() {
         val add: Button = findViewById(R.id.add)
         val reset: Button = findViewById(R.id.reset)
 
+        reset.setOnClickListener{
+            resetList()
+        }
 
         expenseAdapter = CustomExpenseAdapter(expensesList)
         expenses.adapter = expenseAdapter
 
         totalCost.text = Expenses.total(expensesList)
+    }
 
+    private fun resetList(){
+        val builder = AlertDialog.Builder(this)
+        with(builder){
+            setTitle(getString(R.string.confirm_reset))
+            setMessage(getString(R.string.confirm_message))
+
+            setPositiveButton("YES"){
+                dialog, which ->
+                expensesList.clear()
+            }
+
+            setNegativeButton("NO"){
+                dialog, which ->
+            }
+
+            val alertDialog = builder.create()
+            alertDialog.show()
+        }
     }
 }
